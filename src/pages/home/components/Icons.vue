ea949a009304db1dc0c0d3bfc53fa2d047d59aaf
LccAckerman <lccackerman@mails.ccnu.edu.cn>
2019-08-21 21:53:58 +0800

    recommend

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide>
+                <div class="icon" v-for="item of iconList" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl"/>
+                        <p class="icon-desc">{{ item.desc }}</p>
+                    </div>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeIcons",
+    data () {
+        return {
+            iconList: [{
+                id: "0001",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/hotel.png",
+                desc: "酒店"
+            },{
+                id: "0002",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/flight.png",
+                desc: "机票"
+            },{
+                id: "0003",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/package.png",
+                desc: "度假"
+            },{
+                id: "0004",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/train.png",
+                desc: "火车票"
+            },{
+                id: "0005",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/piao.png",
+                desc: "景点门票"
+            },{
+                id: "0006",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/group.png",
+                desc: "特惠酒店"
+            },{
+                id: "0007",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/bargainflight.png",
+                desc: "低价机票"
+            },{
+                id: "0008",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/inn.png",
+                desc: "机票"
+            },{
+                id: "0009",
+                imgUrl: "//s.qunarzz.com/homenode/images/touchheader/haiwai.png",
+                desc: "海外酒店"
+            }]
+        }
+    },
+    computed: {
+        pages (){
+            const pages = []
+            this.iconList.forEach((item, index) => {
+                const page = Math.floor(index/8)
+                if (!pages[page]){
+                    pages[page] = []
+                }
+                pages[page].push(item) 
+            })
+            return pages
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl';
+.icons >>> .swiper-container{
+    height : 0
+    padding-bottom :50% //这样的宽高比是二
+    margin-top :.1rem
+    }
+.icon{
+    position :relative
+    overflow :hidden
+    width :25%
+    height :0
+    float :left
+    padding-bottom :25%
+}
+.icon-img{
+    position :absolute
+    top :0
+    left :0
+    right :0
+    bottom :.44rem
+    box-sizing :border-box
+    padding :.1rem
+}
+.icon-img-content{
+    display :block
+    height :100%
+    margin :0 auto
+    height :100%
+}
+.icon-desc{
+    position :absolute
+    left :0
+    right :0
+    line-height :.44rem
+    height :.44rem
+    color :$darkTextColor
+    text-align :center
+}
+</style>
---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                    <img class="item-img" :src=item.imgUrl />
+                <div class="item-info">
+                    <p class="item-title">{{ item.title}}</p>
+                    <p class="item-desc">{{ item.desc }}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data () {
+        return {
+            recommendList: [{
+            id: '001',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_200x200_99ae30ee.jpg',
+            title:"北京欢乐谷",
+            desc:"亚洲唯一飞行式过山车等你来挑战"
+        },{
+            id: '002',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1804/1a/1a48cc20b0d6ed44a3.img.jpg_200x200_32e22bed.jpg',
+            title:"奥林匹克塔",
+            desc:"今天的拚拚也要元气满满哦"
+        },{
+            id: '003',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+            title:"故宫",
+            desc:"我要超级喜欢你！"
+        }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.title{
+    margin-top :.2rem
+    line-height :.8rem
+    background-color :#eee
+    text-indent :.2rem
+}
+.item{
+    display :flex
+    height :1.9rem
+    overflow :hidden
+}
+.item-img{
+    width :1.7rem
+    height :1.7rem
+    padding :.1rem
+}
+.item-info{
+    flex :1
+    min-width :0
+    padding :.1rem
+}
+.item-title{
+    line-height :.54rem
+    font-size :.32rem
+    text-overflow: ellipsis	
+}
+.item-desc{
+    line-height :.4rem
+    color :#ccc
+    text-overflow: ellipsis
+}
+.item-button{
+    line-height :.44rem
+    background-color #ff9300
+    padding:0 .1rem
+    border-radius :.06rem
+    margin-top :.16rem
+}
+</style>
